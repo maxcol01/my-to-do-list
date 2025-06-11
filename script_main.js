656079d27dcf3abe360a6f68bc1333742c3e18d1
Maxime Collet <maxcollet01@gmail.com>
2025-06-11 13:04:53 +0400

    adding styling to the table

--- script_main.js
+++ script_main.js
@@ -2,7 +2,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 
     // Determining the number of page for the table
-
+    
+    
+    // Navigation of the table element
+    document.getElementById("right-table-btn").addEventListener("click",()=>{
+        alert("Right button clicked !")
+    })
+    
+    
     // Date on the footer
     const dateSpan = document.getElementById("date-footer");
     let date = new Date();
