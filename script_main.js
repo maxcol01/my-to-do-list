5c1237833c99a90ed32f024584818a6d002fac10
Maxime Collet <maxcollet01@gmail.com>
2025-06-13 11:53:19 +0400

    finishing with dark mode toggle, remains the persistence to code

--- script_main.js
+++ script_main.js
@@ -6,11 +6,19 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // Toggling dark mode
     
     toggleDarkModeBtn.addEventListener("click", () =>{
-        toggleDarkModeBtn.classList.remove("fa-toggle-off");
-        toggleDarkModeBtn.classList.add("fa-toggle-on")
-        document.body.classList.add("dark-mode");
-        themeState.innerText = "Light Mode";
-    })
+        if (toggleDarkModeBtn.classList.contains("fa-toggle-off")){
+            toggleDarkModeBtn.classList.remove("fa-toggle-off");
+            toggleDarkModeBtn.classList.add("fa-toggle-on")
+            document.body.classList.add("dark-mode");
+            themeState.innerText = "Light Mode";
+        } else {
+            toggleDarkModeBtn.classList.remove("fa-toggle-on");
+            toggleDarkModeBtn.classList.add("fa-toggle-off")
+            document.body.classList.remove("dark-mode");
+            themeState.innerText = "Dark Mode"; 
+        }
+
+    });
     
     // Determining the number of page for the table
     
@@ -18,7 +26,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // Navigation of the table element
     document.getElementById("right-table-btn").addEventListener("click",()=>{
         alert("Right button clicked !")
-    })
+    });
     
     
     // Date on the footer
